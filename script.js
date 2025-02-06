01dbf837ba0f80da6684790739d7705e951e6d88
ArSi-Anonymous <131711766+ArSi-Anonymous@users.noreply.github.com>
2025-02-06 21:37:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
+    "You're kidding me, right?",
     "Pookie please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
